4f21cb6fcb2fabedda2341844d8bf1a5ae2ac392
HaNhutLinh2000 <60515589+LinhNhut2000@users.noreply.github.com>
2021-08-24 14:31:24 +0700

    feat: init di

---
+++ app/src/main/java/com/example/trackme/utils/di/component/ActivityComponent.kt
@@ -0,0 +1,2 @@
+package com.example.trackme.utils.di.component
+
---
+++ app/src/main/java/com/example/trackme/utils/di/module/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.trackme.utils.di.module
+
+import com.example.trackme.TrackMeApplication
+import com.example.trackme.utils.di.ApplicationScope
+import dagger.Module
+import dagger.Provides
+
+
+@Module
+class AppModule {
+
+//    @ApplicationScope
+//    @Provides
+//    fun provideDatabase(context: TrackMeApplication): TrackMeDatabase {
+//        return Room.databaseBuilder(context, TrackMeDatabase::class.java, "trackme_database")
+//            .build()
+//    }
+
+}
