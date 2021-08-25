a931504ce52f38f3e22f637c7348bfa595401767
MinhNguyen <nhbm1998@gmail.com>
2021-08-25 11:09:14 +0700

    fix permission

--- app/src/main/java/com/example/trackme/view/MapsFragment.kt
+++ app/src/main/java/com/example/trackme/view/MapsFragment.kt
@@ -1,12 +1,16 @@
 package com.example.trackme.view
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.trackme.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -19,15 +23,15 @@ import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 import java.util.jar.Manifest
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), EasyPermissions.PermissionCallbacks {
     private var map: GoogleMap? = null
-
+    private var isDeny = true
     private val callback = OnMapReadyCallback { googleMap ->
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         map = googleMap
-
+        enableLocation()
     }
 
     override fun onCreateView(
@@ -40,11 +44,83 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
 
 
+    fun checkPermission(): Boolean{
+        var res = false
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            res = EasyPermissions.hasPermissions(requireContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            Log.d("TAG", "result SDK < Q and $res")
+        }
+        else{
+            res = EasyPermissions.hasPermissions(requireContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+            Log.d("TAG", "result SDK >= Q and $res")
 
+        }
+        return res
+    }
+
+    fun enableLocation(){
+        if(checkPermission()){
+            map?.isMyLocationEnabled = true
+            isDeny = false
+            return
+        }
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            EasyPermissions.requestPermissions(this,
+                    "This Feature Need To Access Location For Tracking Route",
+                    REQUEST_CODE,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        else{
+            EasyPermissions.requestPermissions(this,
+                    "This Feature Need To Access Location For Tracking Route",
+                    REQUEST_CODE,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+    }
+
+
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        enableLocation()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            AppSettingsDialog.Builder(this).build().show()
+        }
+        else{
+            enableLocation()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,
+                grantResults,this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        enableLocation()
+    }
+
+    companion object{
+        private val REQUEST_CODE = 1
+    }
 }
--- app/src/main/java/com/example/trackme/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/example/trackme/viewmodel/MapViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.trackme.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MapViewModel: ViewModel() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+
+    fun init(context: Context){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    }
 }
